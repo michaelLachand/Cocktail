f6eb94d8eead1e98ea1db00280bc9193a39f0ade
mlachand <lachand_michael@hotmail.com>
2022-03-18 09:29:29 +0100

    ajout component cokctail,public,home

--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { CocktailComponent } from './cocktail/cocktail.component';
+import { ContactComponent } from './contact/contact.component';
+import {PublicRoutingModule} from "./public-routing.module";
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    CocktailComponent,
+    ContactComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    PublicRoutingModule
   ]
 })
 export class PublicModule { }
