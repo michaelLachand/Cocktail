b21f64d3dbad08c829ca9d34e1c96dd2ddf56370
mlachand <lachand_michael@hotmail.com>
2022-03-21 12:33:17 +0100

    ajout auth module css guard compodoc form

--- src/app/admin/cocktail/c-delete/c-delete.component.ts
+++ src/app/admin/cocktail/c-delete/c-delete.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-c-delete',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CDeleteComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private activated: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.activated.params.subscribe(
+      (data) => {
+        console.log(data)
+      }
+    )
   }
 
 }
--- src/app/admin/cocktail/cocktail-routing.module.ts
+++ src/app/admin/cocktail/cocktail-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CIndexComponent} from "./c-index/c-index.component";
+import {CEditComponent} from "./c-edit/c-edit.component";
+import {CAddComponent} from "./c-add/c-add.component";
+import {CDeleteComponent} from "./c-delete/c-delete.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: '', component: CIndexComponent},
+  { path: 'edit/:id', component: CEditComponent},
+  { path: 'add', component: CAddComponent},
+  { path: 'delete/:id', component: CDeleteComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
